a607a367f671d7a63f2194c35c0cfc7b4f82e7ff
krister <krister@polkuapp.com>
2017-06-22 13:04:18 +0300

    Update currentSlide if initialSlide has changed

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -80,6 +80,13 @@ export var InnerSlider = createReactClass({
     }
   },
   componentWillReceiveProps: function(nextProps) {
+
+    if (this.props.initialSlide !== nextProps.initialSlide) {
+      this.setState({
+        currentSlide: nextProps.initialSlide
+      })
+    }
+
     if (this.props.slickGoTo != nextProps.slickGoTo) {
       if (process.env.NODE_ENV !== 'production') {
         console.warn('react-slick deprecation warning: slickGoTo prop is deprecated and it will be removed in next release. Use slickGoTo method instead')
